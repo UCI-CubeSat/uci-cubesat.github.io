23c52c937a38b241fbba538394dac9bfe9846bb9
Abhi Arya <abhigyaa@uci.edu>
2025-01-28 10:46:47 -0800

    trigger build

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -15,6 +15,7 @@ function Home() {
           The UCI CubeSat project is a student-led initiative at the University of California, Irvine to design, build, and launch a CubeSat into low-Earth orbit. The project is a collaboration between the UCI Aerospace Engineering program and the Aerospace Systems and Propulsion Integration Lab (ASPIN) at UCI. The project is led by a team of undergraduate and graduate students from the Henry Samueli School of Engineering at UCI.
           {/* The CubeSat team at UCI is a student-led undergraduate interdisciplinary research and design project with the goal of launching a 2U nanosatellite into orbit to test a UCI research payload. The satellite operates with six main engineering subsystems: Avionics, Communications, Structures, Power, Developer Operations, and Systems, in addition to housing our research payload.
          The goal of the UCI CubeSat project is to design, test, integrate, and launch a modular microsatellite into low-Earth orbit in conjunction with Professor Copp and the ASPIN lab at UCI to test multiple payloads in orbit. The team hopes to be the first student launch at UCI, creating a standard for future student launches and orbital research at UCI.  */}
+         
         </p>
 
         <h3 className="mt-10">Thank you to our Sponsors</h3>
